dfe0a943010679b16cc9ca528b26a0b47600fa4a
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-04-10 15:25:59 +0900

    [feat] bank 하드코딩으로 지정

--- weak1/homework/src/main/java/org/sopt/domain/Bank.java
+++ weak1/homework/src/main/java/org/sopt/domain/Bank.java
@@ -1,6 +1,20 @@
 package org.sopt.domain;
 
 public class Bank {
-    private Long id;
-    private String bankName;
+    private final Long id;
+    private final String bankName;  //현재는 국민은행 하나로 지정했기 때문에 final 선언
+
+    public Bank() {
+        // 국민은행으로 지정
+        this.id = 1L;
+        this.bankName = "국민은행";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
 }
