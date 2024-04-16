32511d04e272ad39257e58e8e2fed88648eb64a5
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-04-16 23:58:18 +0900

    [chore] 폴더명 변경

--- weak1/seminar/src/main/java/org/sopt/classes/Animal.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.classes;
-
-public abstract class Animal {
-    abstract public void walk();
-
-    public void eat() {
-        System.out.println("냠냠");
-    }
-}
--- weak1/seminar/src/main/java/org/sopt/classes/Turtle.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.classes;
-
-public class Turtle extends Animal{
-
-    @Override
-    public void walk() {
-        System.out.println("거북이가 걷습니다.");
-    }
-}
