261b7d9146d18d82ecc6ec60af05e9f930d2c0ef
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-04-26 17:31:53 +0900

    [feat] 3차 세미나 실습 코드

--- week3/src/main/java/com/sopt/demo/respository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sopt.demo.respository;
-import com.sopt.demo.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
--- week3/src/main/java/com/sopt/demo/service/dto/BlogCreateRequest.java
+++
@@ -1,7 +0,0 @@
-package com.sopt.demo.service.dto;
-
-public record BlogCreateRequest (
-        String title,
-        String description
-){
-}
