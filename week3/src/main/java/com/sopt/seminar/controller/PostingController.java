a822ae733bdb4c9383a70501134d60a4b7f34b86
bo-ram-bo-ram <qhfka8253@chungbuk.ac.kr>
2024-05-01 22:08:23 +0900

    [FEAT] postingRequestDto 검증로직 추가

--- week3/src/main/java/com/sopt/seminar/controller/PostingController.java
+++ week3/src/main/java/com/sopt/seminar/controller/PostingController.java
@@ -4,6 +4,7 @@ import com.sopt.seminar.common.dto.SuccessMessage;
 import com.sopt.seminar.common.dto.SuccessStatusResponse;
 import com.sopt.seminar.service.PostingService;
 import com.sopt.seminar.service.dto.PostingCreateRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class PostingController {
     @PostMapping
     public ResponseEntity<SuccessStatusResponse> createPosting(
             @RequestHeader(name = "memberId")  Long memberId,
-            @RequestBody PostingCreateRequest postingCreateRequest
+            @Valid @RequestBody PostingCreateRequest postingCreateRequest
     ){        return ResponseEntity.status(HttpStatus.CREATED)
             .header("postingId", postingService.createPosting(memberId, postingCreateRequest))
             .body(SuccessStatusResponse.of(SuccessMessage.POSTING_CREATE_SUCCESS));
